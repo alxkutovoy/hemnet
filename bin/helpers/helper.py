49954d206d02377f2356158dd0e7371e50466c95
Alexander Kutovoy <alxkutovoy@gmail.com>
2020-09-07 23:27:00 +0200

    Fix minor typos

--- bin/helpers/helper.py
+++ bin/helpers/helper.py
@@ -147,11 +147,11 @@ class Helper:
     def gcs_to_dist(self, point_a, point_b):
         lat_a, lon_a = self.radians(float(point_a[0])), self.radians(float(point_a[1]))
         lat_b, lon_b = self.radians(float(point_b[0])), self.radians(float(point_b[1]))
-        R = 6378.137  # Radius of Earth in km
+        r = 6378.137  # Radius of Earth in km
         dlon, dlat = lon_b - lon_a, lat_b - lat_a
         a = self.sin(dlat / 2) ** 2 + self.cos(lat_a) * self.cos(lat_b) * self.sin(dlon / 2) ** 2
         c = 2 * self.atan2(self.sqrt(a), self.sqrt(1 - a))
-        distance = R * c * 1000  # Distance in meters
+        distance = r * c * 1000  # Distance in meters
         return distance
 
     def json_to_df(self, path):
