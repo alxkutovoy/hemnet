ac0af5dc0f358d8b80f9fea4156029a03331db09
Alexander Kutovoy <alxkutovoy@gmail.com>
2020-09-22 15:29:08 +0200

    Abstract data extraction

--- bin/model/feature_selection.py
+++ bin/model/feature_selection.py
@@ -6,17 +6,17 @@ class FeatureSelection(object):
     import seaborn as sns
 
     from lightgbm.sklearn import LGBMRegressor
-    from os import path
-    from pathlib import Path
 
-    from bin.helpers.helper import Helper
-    from utils.files import Utils
+    from utils.data import Data
+    from utils.var import File
+    from utils.io import IO
 
     def feature_selection(self, threshold=0.99, criterion='neg_root_mean_squared_error'):
         from sklearn.inspection import permutation_importance
         print('\nSelect features:')
         # Get data
-        x_train, x_val, y_train, y_val = self._get_processed_data()
+        data = self.Data().processed()
+        x_train, x_val, y_train, y_val = data['x_train'], data['x_val'], data['y_train'], data['y_val']
         # Define classifier and fit data
         print('\n\tDefine classifier and fit data...', end=' ', flush=True)
         pipe = self.LGBMRegressor()
@@ -35,28 +35,6 @@ class FeatureSelection(object):
         self._select_features(features)
         print('\nFeature selection was successfully completed.')
 
-    def _get_processed_data(self):
-        helper, utils = self.Helper(), self.Utils()
-        # Get path
-        x_train_path = utils.get_full_path('data/dataset/processed/x_train')
-        y_train_path = utils.get_full_path('data/dataset/processed/y_train')
-        x_val_path = utils.get_full_path('data/dataset/processed/x_val')
-        y_val_path = utils.get_full_path('data/dataset/processed/y_val')
-        # Check if exists
-        exists = all([helper.exists(x_train_path),
-                      helper.exists(y_train_path),
-                      helper.exists(x_val_path),
-                      helper.exists(y_val_path)])
-        if not exists:
-            print('Sorry, datasets does not exist. Please, run preprocessing first.')
-            return
-        # Convert to DataFrame
-        x_train = self.pd.read_parquet(x_train_path, engine="fastparquet")
-        y_train = self.pd.read_parquet(y_train_path, engine="fastparquet")
-        x_val = self.pd.read_parquet(x_val_path, engine="fastparquet")
-        y_val = self.pd.read_parquet(y_val_path, engine="fastparquet")
-        return x_train, x_val, y_train, y_val
-
     def _evaluate(self, results, threshold, columns):
         print('\tProcess results...', end=' ', flush=True)
         output = self.pd.DataFrame({'feature': columns, 'importance': results.importances_mean}) \
@@ -70,25 +48,23 @@ class FeatureSelection(object):
 
     def _illustrator(self, features):
         print('\tPlot results...', end=' ', flush=True)
-        utils = self.Utils()
+        io = self.IO()
         figure, ax = self.plt.subplots(figsize=(10, 100), dpi=350)
         self.sns.barplot(data=features, y='feature', x='importance', palette="RdBu", hue='selected')
         self.sns.set_style(style='darkgrid')
         ax.set_title('Permutation feature importance')
-        directory = utils.get_full_path('data/dataset/reporting')
-        path = self.path.join(directory, 'feature_importance.png')
-        self.Path(directory).mkdir(parents=True, exist_ok=True)
+        path = self.File.FEATURE_IMPORTANCE_REPORT
+        io.make_dir(io.dir(path))
         figure.savefig(path, dpi=350, bbox_inches='tight')
         self.plt.close()
         print('Done.')
 
     def _select_features(self, features):
         print('\tSelect features...', end=' ', flush=True)
-        utils = self.Utils()
+        io = self.IO()
         selected_features = features[features.selected].feature.tolist()
-        directory = utils.get_full_path('data/dataset/processed/operations')
-        path = self.path.join(directory, 'features.txt')
-        self.Path(directory).mkdir(parents=True, exist_ok=True)
+        path = self.File.SELECTED_FEATURES
+        io.make_dir(io.dir(path))
         with open(path, 'w') as f:
             f.write('\n'.join(selected_features))
         print('Done.')
